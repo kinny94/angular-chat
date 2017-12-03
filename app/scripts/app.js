106905dccf7950a16a1078609ee2f36f0d77b9c9
kinny94 <ad.sam007@gmail.com>
2017-12-03 15:30:51 -0500

    Added boilerplate

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularChatApp
+ * @description
+ * # angularChatApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularChatApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularChatApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularChatApp
+ */
+angular.module('angularChatApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
