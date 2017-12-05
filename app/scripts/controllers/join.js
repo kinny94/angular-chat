532c22bda4355a9d146b02c674da0e50268f18e1
kinny94 <ad.sam007@gmail.com>
2017-12-04 23:56:04 -0500

    Comments

--- app/scripts/controllers/join.js
+++ app/scripts/controllers/join.js
@@ -1,12 +1,5 @@
 'use strict';
 
-/**
-* @ngdoc function
-* @name angularChatApp.controller:JoinCtrl
-* @description
-* # JoinCtrl
-* Controller of the angularChatApp
-*/
 
 angular.module('angularChatApp')
     .controller('JoinCtrl', function(){
