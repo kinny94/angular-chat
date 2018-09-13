670816d9e4df6c50faa7516ed9fb6081bb77bca6
kinny94 <ad.sam007@gmail.com>
2018-09-13 11:13:41 -0400

    added components

--- src/app/user-selection/user-selection.component.ts
+++ src/app/user-selection/user-selection.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-user-selection',
-  templateUrl: './user-selection.component.html',
-  styleUrls: ['./user-selection.component.css']
+	selector: 'app-user-selection',
+	templateUrl: './user-selection.component.html',
+	styleUrls: ['./user-selection.component.css']
 })
 export class UserSelectionComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
