c18d289d081fe082f94715303299f6379dffac6c
Arjun Dass <ad.sam007@gmail.com>
2018-09-21 11:53:54 -0400

    rxjs debugging tools

--- src/app/services/effects/load-threads-effect.service.ts
+++ src/app/services/effects/load-threads-effect.service.ts
@@ -2,7 +2,7 @@ import { LOAD_USER_THREADS_ACTION, UserThreadsLoadedActions } from './../../stor
 import { ThreadsService } from './../threads.service';
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { switchMap, map } from 'rxjs/operators';
+import { switchMap, map, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Action } from '@ngrx/store';
 
@@ -17,6 +17,7 @@ export class LoadThreadsEffectService {
 
 	@Effect() userThreads$: Observable<Action> = this.actions$.pipe(
 		ofType( LOAD_USER_THREADS_ACTION ),
+		tap( val => console.log( "action received", val )),
 		switchMap(() => this.threadService.loadUserThreads()),
 		map( allUserData => new UserThreadsLoadedActions( allUserData ))
 	)
