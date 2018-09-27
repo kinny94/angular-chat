c527037d5c93f407a1f3d822f356f8642d379336
Arjun Dass <ad.sam007@gmail.com>
2018-09-27 15:52:33 -0400

    fixing builds

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,15 +1,18 @@
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
+const path = require('path');
 
-var { apiGetUserThreads } = require('./api/apiGetUserThreads' );
+var { apiGetUserThreads } = require( './api/apiGetUserThreads' );
 var { apiUpdateThread } = require( './api/apiMarkThreadAsReadByUser' );
 var { apiMessageNotificationsPerUser } = require('./api/apiMessageNotificationsPerUser' );
 var { apiSaveNewMessage } = require('./api/apiSaveNewMessage' );
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 3001;
+app.set('port', port);
 
 app.use( bodyParser.json());
 
